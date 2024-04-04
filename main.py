d029b20f4f76f7d433a452c4977a79cc0d0ec339
gh0st <maratka2202@gmail.com>
2024-04-04 16:34:18 +0300

    Project settings init

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from settings import setting
+
+
+@asynccontextmanager
+async def project_lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(
+    title=setting.app_name,
+    debug=setting.debug,
+    summary=setting.summary,
+    description=setting.description,
+    
+    docs_url=setting.docs_url,
+    openapi_url=setting.openapi_url,
+    
+    lifespan=project_lifespan,
+)
--- settings.py
+++ settings.py
@@ -1,5 +1,29 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSetting(BaseSettings):
+    USER: str
+    PASSWORD: str
+    HOST: str
+    NAME: str
+    PORT: str
+    
+    dsn: str = f"postgresql+aiopg://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 
 class Setting(BaseSettings):
-    pass
+    app_name: str = "My Library API"
+    debug: bool = True
+    description: str = "Easy and Fast api to control library"
+    summary: str = "Easy api to library"
+    
+    db: DatabaseSetting = DatabaseSetting()
+    
+    docs_url: str = "/api/docs"
+    openapi_url: str = "/api/openapi.json"
+
+
+setting = Setting()
